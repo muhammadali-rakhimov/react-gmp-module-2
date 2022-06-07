115bf90fa1b978b921a1a7be7b9fe34ded516925
Muhammadali Rakhimov <muhammadali.rakhimov786@gmail.com>
2022-06-08 01:47:24 +0500

    Upd

--- src/features/AddMovieModal/AddMovieModal.jsx
+++ src/features/AddMovieModal/AddMovieModal.jsx
@@ -1,3 +1,7 @@
+/* eslint-disable react/jsx-curly-brace-presence */
+/* eslint-disable quotes */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable react/self-closing-comp */
 /* eslint-disable react/button-has-type */
 import React from 'react'
 import styled from './styles.module.scss'
@@ -6,8 +10,20 @@ import styled from './styles.module.scss'
 function AddMovieModal({ clickClose }) {
   return (
     <div className={styled.parent}>
-      AddMovieModal
-      <button onClick={clickClose}>Close modal</button>
+      <div className={styled.titleDiv}>
+        <h2>ADD MOVIE</h2>
+        <button onClick={clickClose}>&#10006;</button>
+      </div>
+      <form className={`col-lg-12`}>
+        <div className={`col-5 ${styled.inputLabel}`}>
+          <label htmlFor="modalName">TITLE</label>
+          <input id="modalName" type="text" />
+        </div>
+        <div className={`col-5 ${styled.inputLabel}`}>
+          <label htmlFor="modalName">TITLE</label>
+          <input id="modalName" type="text" />
+        </div>
+      </form>
     </div>
   )
 }
